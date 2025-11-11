3a629d1db6af9b0ccde4c80909d028027f11a6ef
Lucas. <iaminvisy@gmail.com>
2025-11-11 12:00:36 +0000

    Init commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  build: { format: 'preserve' },
+  trailingSlash: 'never',
+  site: 'https://pi.invisyarcticfox.uk',
+  server: {
+    port: 4321,
+    allowedHosts: [ 'raspi' ]
+  }
+})
