cc2234b19f07ed599a096236e5b1e8a4a624e651
Lucas. <iaminvisy@gmail.com>
2025-11-12 01:19:41 +0000

    server stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import node from '@astrojs/node';
+
 
 // https://astro.build/config
 export default defineConfig({
   build: { format: 'preserve' },
+  output: 'server',
   trailingSlash: 'never',
   site: 'https://pi.invisyarcticfox.uk',
+
   server: {
+    host: '0.0.0.0',
     port: 4321,
-    allowedHosts: [
-      'raspi',
-      'pi.invisyarcticfox.uk',
-    ]
-  }
+    allowedHosts: [ 'raspi', 'pi.invisyarcticfox.uk' ]
+  },
+
+  adapter: node({ mode: 'standalone' })
 })
